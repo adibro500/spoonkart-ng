4b1de8f46e50011018ab0d5da5db959807eea0ef
Aditya Ammanabrolu <ammanabrolu.aditya@gmail.com>
2018-01-19 19:43:55 +0530

    bbb

--- src/app/dashboard/customer-info/customer-service.ts
+++ src/app/dashboard/customer-info/customer-service.ts
@@ -4,19 +4,25 @@ import { options } from "../../Headers";
 import 'rxjs/add/operator/map';
 
 @Injectable()
-export class CustomerService{
+export class CustomerService {
 
-    constructor(private http:Http){
+    constructor(private http: Http) {
 
     }
-    saveCustomerInput(data){
+    saveCustomerInput(data) {
         console.log("inputdata", data);
-        
-        return this.http.post("https://spoonkart-service.herokuapp.com/save/customer", data, options) .map(res => res.json()).subscribe(data => console.log(data));
-        
+
+        return this.http.post("https://spoonkart-service.herokuapp.com/save/customer", data, options).map(res => res.json()).subscribe(data => console.log(data));
+
+
+    }
+    updateCustomerInput(str1, data) {
+
+        return this.http.post("https://spoonkart-service.herokuapp.com/update/" + str1, data, options).map(res => res.json()).subscribe(data => console.log(data));
+
 
     }
 
-   
+
 
 }
--- src/app/dashboard/grid/grid.component.ts
+++ src/app/dashboard/grid/grid.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
+import { DatatableComponent, TableColumn } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'grid',
   template: `
     <div>
-      
+      <div class="text-right">
+        <button type="button" class="btn1 btn sv_btn" (click)="exportAsCSV()" >
+              <label>
+                 Export to CSV
+              </label>
+          </button>
+      </div>
       <ngx-datatable
-        class="material"
+      #dataTable  
+      class="material"
         [rows]="rows"
         [loadingIndicator]="loadingIndicator"
-        [columns]="cols"
         [columnMode]="'force'"
         [headerHeight]="50"
         [footerHeight]="50"
         [rowHeight]="'auto'"
         [reorderable]="reorderable"
         [limit]="10">
+       <ng-container *ngFor="let col of cols;let j=index">
+                    <ng-container *ngIf="col!==undefined">
+                     
+                  <ngx-datatable-column prop={{col}} [sortable]="true" [name]="col"  [resizeable]="true" [canAutoResize]="true">        
+                  </ngx-datatable-column>
+                    </ng-container>
+               </ng-container>  
       </ngx-datatable>
      
     </div>
@@ -27,18 +42,17 @@ export class GridComponent {
   loadingIndicator: boolean = true;
   reorderable: boolean = true;
 
-  cols:any =[ {name:'_id'},{name:'first name'},{name:'last name'}];
+  cols = ['Customer ID', 'first name', 'last name', 'house address', 'flat address'];
 
-  constructor() {
+  constructor(public changeDetectorRef: ChangeDetectorRef) {
     this.fetch((data) => {
       this.rows = data;
-      setTimeout(() => { this.loadingIndicator = false; }, 1500);
     });
   }
 
   fetch(cb) {
     const req = new XMLHttpRequest();
-    req.open('GET', `http://localhost:5000/get/customers`);
+    req.open('GET', `https://spoonkart-service.herokuapp.com/get/customers`);
 
     req.onload = () => {
       cb(JSON.parse(req.response));
@@ -46,5 +60,48 @@ export class GridComponent {
 
     req.send();
   }
+
+
+  @ViewChild('dataTable') public dataTable: DatatableComponent;
+
+  exportAsCSV() {
+    const columns: TableColumn[] = this.dataTable.columns;
+    const headers =
+      columns
+        .map((column: TableColumn) => column.name)
+        .filter((e) => e);  // remove column without name (i.e. falsy value)
+
+    const rows: any[] = this.dataTable.rows.map((row) => {
+      let r = {};
+      columns.forEach((column) => {
+        if (!column.name) { return; }   // ignore column without name
+        if (column.prop) {
+          let prop = column.prop;
+          r[prop] = (typeof row[prop] === 'boolean') ? (row[prop]) ? 'Yes'
+            : 'No'
+            : row[prop];
+        } else {
+          // special cases handled here
+        }
+      })
+      return r;
+    });
+
+    const options = {
+      fieldSeparator: ',',
+      quoteStrings: '"',
+      decimalseparator: '.',
+      showLabels: true,
+      headers: headers,
+      showTitle: false,
+      title: 'Report',
+      useBom: true,
+    };
+    return new Angular2Csv(rows, 'report', options);
+  }
+
+
+
+
 
 }
